db80ff44af48aa391841e735bb11c4092c93378f
김민수 <danbikma@naver.com>
2022-12-09 02:19:17 +0900

    백준9498

---
+++ 9498.py
@@ -0,0 +1,13 @@
+result = input()
+result = int(result)
+
+if 90 <= result <= 100:
+    print("A")
+elif result >= 80:
+    print("B")
+elif result >= 70:
+    print("C")
+elif result >= 60:
+    print("D")
+elif result >=0:
+    print("F")
